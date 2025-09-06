d4d7f05832cb246980daa251db92359341f9a8c3
Kelly Seeman <kseeman123@gmail.com>
2025-09-06 18:17:31 -0500

    Updates ESLint configuration for ignores
    Updates the ESLint configuration to correctly handle ignored files.
    
    This change ensures that specified files and directories are properly excluded from ESLint's analysis by using a more explicit configuration format.

--- apps/web/eslint.config.js
+++ apps/web/eslint.config.js
@@ -5,7 +5,7 @@ import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
 
 export default tseslint.config([
-  globalIgnores(['dist']),
+  { ignores: ['dist'] },
   {
     files: ['**/*.{ts,tsx}'],
     extends: [
